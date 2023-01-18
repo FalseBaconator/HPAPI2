586960864b1fd0d2df495d7e01595ec0f45b3506
Ryan Wohler <ryanwohler55@gmail.com>
2023-01-18 09:30:00 -0400

    added pseudo respawn

--- HPAPI2/Player.cs
+++ HPAPI2/Player.cs
@@ -9,11 +9,14 @@ namespace HPAPI2
     internal class Player
     {
         int HP;
+        int maxHP;
         int Lives;
+
 
         public Player(int hP = 100, int lives = 3)
         {
             HP = hP;
+            maxHP = hP;
             Lives = lives;
         }
 
@@ -21,6 +24,14 @@ namespace HPAPI2
         {
             Console.WriteLine("take " + dmg + " dmg");
             HP -= dmg;
+            if(HP <= 0 && Lives > 0)
+            {
+                Lives--;
+                HP = maxHP;
+            }else if (HP <= 0)
+            {
+                Console.WriteLine("player has died");
+            }
         }
 
         public void Heal(int heal)
--- HPAPI2/Program.cs
+++ HPAPI2/Program.cs
@@ -6,8 +6,6 @@ using System.Threading.Tasks;
 
 namespace HPAPI2
 {
-
-
     internal class Program
     {
         static void Main(string[] args)
@@ -15,11 +13,18 @@ namespace HPAPI2
             Console.WriteLine("HP API 2.0");
             Player player = new Player(100, 3);
             player.ShowStats();
-            player.TakeDMG(5);
+            player.TakeDMG(50);
             player.ShowStats();
-            player.Heal(3);
+            player.Heal(25);
+            player.ShowStats();
+            player.TakeDMG(100);
             player.ShowStats();
             Console.ReadKey(true);
         }
+
+
+
+
+
     }
 }
